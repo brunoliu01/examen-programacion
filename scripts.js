20667ebdbd7594737c0fca04680339341444695b
Alumno03 s604 <a03s604@campustls.local>
2016-09-23 16:01:03 -0500

    añado primera parte

---
+++ scripts.js
@@ -0,0 +1,142 @@
+var a = 5;
+var b = 3;
+var c = -12;
+
+/* EJERCICIO A */
+
+if (a>3) {
+	console.log("correcto")
+	}
+else{
+	console.log("incorrecto")
+}	
+
+
+/* EJERCICIO B */
+
+if (a>c) {
+	console.log("correcto")
+}
+else{
+	console.log("incorrecto")
+}
+
+/* EJERCICIO C */
+
+if (a<c) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+	}
+
+	
+/* EJERCICIO D */
+if (b<c) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}
+
+
+
+if (b!=c) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}
+
+
+	
+	if (a==3) {
+	console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}
+
+
+
+if (a*b==15) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}		
+
+
+
+if (a*b==-30) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}		
+
+
+
+if (c/b < a) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}		
+
+
+
+
+if (c/b == -10) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}		
+
+
+
+if (c/b == -4) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}		
+
+
+
+if (a+b+c == 5) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}		
+
+		var suma = +a + +b;
+		var resta = +a - +b;
+
+if (suma == 8  && resta == 2){
+		console.log("correcto")
+		}
+
+else{
+		console.log("incorrecto")
+		}		
+
+
+
+if (suma == 8  || resta == 6){
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}		
+
+
+ if (a>3 && b>3 && c<3 ) {
+		console.log("correcto")
+		}
+else{
+		console.log("incorrecto")
+		}		
+
